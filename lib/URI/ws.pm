346ccd30a2bcfa72ff22842d787aeb345e91062a
Codacy Archive <codacy-archive@local>
2025-11-07 15:40:57 +0000

    snapshot 2025-11-07T15:40:57.499158+00:00

--- lib/URI/ws.pm
+++ lib/URI/ws.pm
@@ -1,41 +1,32 @@
-package URI::icap;
+package URI::ws;
 
 use strict;
 use warnings;
-use base qw(URI::http);
 
 our $VERSION = '5.35';
 
-sub default_port { return 1344 }
+use parent 'URI::http';
 
 1;
 __END__
 
 =head1 NAME
 
-URI::icap - URI scheme for ICAP Identifiers
-
-=head1 VERSION
-
-Version 5.20
+URI::ws - URI scheme for WebSocket Identifiers
 
 =head1 SYNOPSIS
 
-    use URI::icap;
+    use URI::ws;
 
-    my $uri = URI->new('icap://icap-proxy.example.com/');
+    my $uri = URI->new('ws://example.com/');
 
 =head1 DESCRIPTION
 
-This module implements the C<icap:> URI scheme defined in L<RFC 3507|http://tools.ietf.org/html/rfc3507>, for the L<Internet Content Adaptation Protocol|https://en.wikipedia.org/wiki/Internet_Content_Adaptation_Protocol>.
+This module implements the C<ws:> URI scheme defined in L<RFC 6455|http://tools.ietf.org/html/rfc6455>.
 
 =head1 SUBROUTINES/METHODS
 
-This module inherits the behaviour of L<URI::http|URI::http> and overrides the L<default_port|URI#$uri->default_port> method.
-
-=head2 default_port
-
-The default port for icap servers is 1344
+This module inherits the behaviour of L<URI::http|URI::http>.
 
 =head1 DIAGNOSTICS
 
@@ -59,7 +50,7 @@ See L<URI|URI#BUGS>
 
 =head1 SEE ALSO
 
-L<RFC 3507|http://tools.ietf.org/html/rfc3507>
+L<RFC 6455|http://tools.ietf.org/html/rfc6455>
 
 =head1 AUTHOR
 
